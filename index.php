cfcde581c3ff501d36bb3416ab3c8fb8f8da0441
Richard Lewis <richard@re.adyma.de>
2015-08-01 19:21:27 -0400

    style loop-thumbnail grid

--- index.php
+++ index.php
@@ -1,35 +1,19 @@
 <?php get_header(); ?>	
 <?php $shortname = "neue"; ?>
-<div id="content">
-	<div id="content_inside">
-    <?php
-    global $wp_query;
-        
-    $args = array_merge( $wp_query->query, array( 'posts_per_page' => 12 ) );
-    query_posts( $args );        
+<main class="l-main" role="main">
+
+	<div class="l-site-wrapper-inner">
+
+	    <?php
+	    global $wp_query;
+	        
+	    $args = array_merge( $wp_query->query, array( 'posts_per_page' => 12 ) );
+	    query_posts( $args );        
+	    
+	    get_template_part( 'partials/loop', 'thumbnails' ); ?>  
+	    
+    </div>                          
     
-    get_template_part( 'partials/loop', 'thumbnails' ); ?>  
-    
-    
-    </div><!--//content_inside-->
-    
-    <div class="load_more_cont">
-        <div align="center"><div class="load_more_text">
-        
-        <?php
-        
-        ob_start();
-		next_posts_link('<img src="' . get_bloginfo('stylesheet_directory') . '/images/loading-button.png" />');
-		$buffer = ob_get_contents();
-		ob_end_clean();	
-		if(!empty($buffer)) echo $buffer;
-        ?>
-        
-        </div></div>
-    </div><!--//load_more_cont-->                    
-    
-    <?php wp_reset_query(); ?>                            
-    
-</div><!--//content-->    
-	
+  
+</main>
 <?php get_footer(); ?>
--- partials/loop-thumbnails.php
+++ partials/loop-thumbnails.php
@@ -1,25 +1,47 @@
-<? while (have_posts()) : the_post(); ?>                        
+<div class="load_more_cont">
+	        
+    <?php
     
-    <a href="<?php the_permalink(); ?>" class="loop-thumbnail-wrapper">
-	
-		<?php the_post_thumbnail('home-image', array('class' => 'loop-thumbnail-image' )); ?>
+    ob_start();
+	next_posts_link('next');
+	$buffer = ob_get_contents();
+	ob_end_clean();	
+	if(!empty($buffer)) echo $buffer;
+    ?>
+                        
 
-		<div class="loop-thumbnail-meta meta-data">
-			<div class="meta-category color-highlight">
-				<?php
-					foreach((get_the_category()) as $category) { 
-					    echo $category->cat_name . ' '; 
-					} 
-				?>
-			</div>
-			<h3><?php the_title(); ?></h3>
-			<?php if( get_field('tagline') ): ?>
-				<div class="meta-tagline"><?php the_field('tagline'); ?></div>
-			<?php endif; ?>
-			<div class="meta-date">
-				<?php the_date(); ?>
-			</div>
-		</div>
-	</a>
+	<?php wp_reset_query(); ?>  
+</div>
 
-<?php endwhile; ?>
+<div class="loop-thumbnail-wrapper">
+	<? while (have_posts()) : the_post(); ?>                        
+	    
+	    <a href="<?php the_permalink(); ?>" class="loop-thumbnail-item apply-vertical-rhythm">
+		
+			<?php the_post_thumbnail('home-image', array('class' => 'loop-thumbnail-image' )); ?>
+
+			<div class="loop-thumbnail-meta meta-data">
+				<div class="meta-category color-highlight">
+					<?php
+						$categories = get_the_category();
+						foreach($categories as $key => $category) { 
+						    if ($categories[$key + 1]) { // if there's a category+1, ech a comma
+		 					    echo $category->cat_name . ', '; 
+						    } else { // if not, no comma
+							    echo $category->cat_name . ' '; 
+						    }
+						} 
+					?>
+				</div>
+				<h2><?php the_title(); ?></h2>
+				<?php if( get_field('tagline') ): ?>
+					<div class="meta-tagline"><?php the_field('tagline'); ?></div>
+				<?php endif; ?>
+				<div class="meta-date">
+					<?php the_date(); ?>
+				</div>
+			</div>
+		</a>
+
+	<?php endwhile; ?>
+</div>
