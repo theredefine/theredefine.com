db8e4b1b94203344d6e41f1af29191f1f5bc51b7
Richard Lewis <richard@re.adyma.de>
2015-08-01 13:07:23 -0400

    initial commit...bring this stuff under version control

---
+++ footer.php
@@ -0,0 +1,14 @@
+	<div class="clear"></div>	
+	
+	<div id="footer">
+	
+		
+		
+		
+		<div class="footer_text">  </div>
+	
+	</div><!--//footer-->
+</div><!--//main_container-->
+<?php wp_footer(); ?>
+</body>
+</html>
