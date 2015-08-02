87b9c4ff3909c08326d184a41a0462bb4d7a96bf
Richard Lewis <richard@re.adyma.de>
2015-08-01 22:22:00 -0400

    update layouts of single and page templates

--- single.php
+++ single.php
@@ -1,22 +1,23 @@
 <?php get_header(); ?>	
 	<main class="l-main" role="main">
-	
-		<section class="l-page-content">
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>          
-				<h1 class="single_title"><?php the_title(); ?></h1>
-				<div class="single_inside_content">
-				<?php the_content(); ?>
-				</div><!--//single_inside_content-->
-				<br /><br />
-				<?php //comments_template(); ?>
-			<?php endwhile; else: ?>
-				<h3>Sorry, no posts matched your criteria.</h3>
-			<?php endif; ?>                    
-		</section>
-		
+		<div class="l-site-wrapper-inner ">
+			<section class="l-page-content">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>          
+					<h1 class="single_title"><?php the_title(); ?></h1>
+					<div class="single_inside_content">
+					<?php the_content(); ?>
+					</div><!--//single_inside_content-->
+					<br /><br />
+					<?php //comments_template(); ?>
+				<?php endwhile; else: ?>
+					<h3>Sorry, no posts matched your criteria.</h3>
+				<?php endif; ?>                    
+			</section>
+			
 
-		
-		<?php get_sidebar(); ?>
+			
+			<?php get_sidebar(); ?>
+		</div>
 	
 	</main><!--//single_cont-->
 	
